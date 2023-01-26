e2355049ac5274e3545e894d5707509a3ba7b3a4
steven2308 <steven2308@outlook.com>
2023-01-26 18:22:48 -0500

    Lesson 2: Adding assets.

--- contracts/SnakeSoldier.sol
+++ contracts/SnakeSoldier.sol
@@ -5,14 +5,19 @@ import "@rmrk-team/evm-contracts/contracts/RMRK/access/Ownable.sol";
 import "@rmrk-team/evm-contracts/contracts/RMRK/equippable/RMRKEquippable.sol";
 import "@rmrk-team/evm-contracts/contracts/RMRK/extension/RMRKRoyalties.sol";
 import "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKCollectionMetadata.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract SnakeSoldier is
     Ownable,
     RMRKCollectionMetadata,
     RMRKRoyalties,
-    RMRKEquippable {
+    RMRKEquippable
+{
+    using Strings for uint256;
 
     string private _defaultTokenUri;
+    uint256 private _totalAssets;
+    mapping(uint64 => uint256) private _isTokenAssetEnumerated;
 
     constructor(
         string memory collectionMetadata_,
@@ -30,8 +35,83 @@ contract SnakeSoldier is
     ) external override onlyOwner {
         _setRoyaltyRecipient(newRoyaltyRecipient);
     }
-    
+
     function tokenURI(uint256) public view returns (string memory) {
         return _defaultTokenUri;
+    }
+
+    function addAssetEntry(
+        string memory metadataURI
+    ) public virtual onlyOwnerOrContributor returns (uint256) {
+        unchecked {
+            _totalAssets += 1;
+        }
+        _addAssetEntry(uint64(_totalAssets), metadataURI);
+        return _totalAssets;
+    }
+
+    function addEquippableAssetEntry(
+        uint64 equippableGroupId,
+        address catalogAddress,
+        string memory metadataURI,
+        uint64[] calldata partIds
+    ) public virtual onlyOwnerOrContributor returns (uint256) {
+        unchecked {
+            _totalAssets += 1;
+        }
+        _addAssetEntry(
+            uint64(_totalAssets),
+            equippableGroupId,
+            catalogAddress,
+            metadataURI,
+            partIds
+        );
+        return _totalAssets;
+    }
+
+    function addAssetToToken(
+        uint256 tokenId,
+        uint64 assetId,
+        uint64 replacesAssetWithId
+    ) public virtual onlyOwnerOrContributor {
+        _addAssetToToken(tokenId, assetId, replacesAssetWithId);
+        if (_msgSender() == ownerOf(tokenId)) {
+            _acceptAsset(tokenId, _pendingAssets[tokenId].length - 1, assetId);
+        }
+    }
+
+    function setValidParentForEquippableGroup(
+        uint64 equippableGroupId,
+        address parentAddress,
+        uint64 partId
+    ) public virtual onlyOwnerOrContributor {
+        _setValidParentForEquippableGroup(
+            equippableGroupId,
+            parentAddress,
+            partId
+        );
+    }
+
+    function getAssetMetadata(
+        uint256 tokenId,
+        uint64 assetId
+    )
+        public
+        view
+        override(AbstractMultiAsset, IRMRKMultiAsset)
+        returns (string memory)
+    {
+        string memory metaUri = super.getAssetMetadata(tokenId, assetId);
+        if (_isTokenAssetEnumerated[assetId] != 0)
+            metaUri = string(abi.encodePacked(metaUri, tokenId.toString()));
+        return metaUri;
+    }
+
+    function setAssetEnumerated(
+        uint64 assetId,
+        bool enumerated
+    ) external onlyOwner {
+        if (enumerated) _isTokenAssetEnumerated[assetId] = 1;
+        else delete _isTokenAssetEnumerated[assetId];
     }
 }

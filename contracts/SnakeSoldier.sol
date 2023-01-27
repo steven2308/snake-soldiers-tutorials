174bfeff04849bb8530b5043915923fca607ebbd
steven2308 <steven2308@outlook.com>
2023-01-26 20:01:09 -0500

    Lesson 3: Setting up Tiers (Ranks) and Phases for mint.

--- contracts/SnakeSoldier.sol
+++ contracts/SnakeSoldier.sol
@@ -7,6 +7,9 @@ import "@rmrk-team/evm-contracts/contracts/RMRK/extension/RMRKRoyalties.sol";
 import "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKCollectionMetadata.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+error MaxPhaseReached();
+error NextPhasePriceMustBeEqualOrHigher();
+
 contract SnakeSoldier is
     Ownable,
     RMRKCollectionMetadata,
@@ -14,6 +17,25 @@ contract SnakeSoldier is
     RMRKEquippable
 {
     using Strings for uint256;
+
+    enum Rank {
+        Soldier,
+        Commander,
+        General
+    }
+
+    uint256 private constant _MAX_SUPPLY_PER_PHASE_SOLDIERS = 1200; // A maximum possible of 1200*4=4800
+    uint256 private constant _MAX_SUPPLY_PER_PHASE_COMMANDERS = 45; // A maximum possible of 45*4=180
+    uint256 private constant _MAX_SUPPLY_PER_PHASE_GENERALS = 5; // A maximum possible of 5*4=20
+    uint256 private constant _MAX_PHASES = 4;
+
+    uint256 private _pricePerSoldier;
+    uint256 private _pricePerCommander;
+    uint256 private _pricePerGeneral;
+
+    mapping(Rank => uint256) private _totalSupply;
+    uint256 private _phase;
+    bool private _phasesLocked;
 
     string private _defaultTokenUri;
     uint256 private _totalAssets;
@@ -113,5 +135,54 @@ contract SnakeSoldier is
     ) external onlyOwner {
         if (enumerated) _isTokenAssetEnumerated[assetId] = 1;
         else delete _isTokenAssetEnumerated[assetId];
+    }
+
+    function totalSupply(Rank rank) public view returns (uint256) {
+        return _totalSupply[rank];
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return
+            _totalSupply[Rank.Soldier] +
+            _totalSupply[Rank.Commander] +
+            _totalSupply[Rank.General];
+    }
+
+    function maxSupply(Rank rank) public view returns (uint256) {
+        if (rank == Rank.Soldier)
+            return _MAX_SUPPLY_PER_PHASE_SOLDIERS * _phase;
+        else if (rank == Rank.Commander)
+            return _MAX_SUPPLY_PER_PHASE_COMMANDERS * _phase;
+        else return _MAX_SUPPLY_PER_PHASE_GENERALS * _phase;
+    }
+
+    function maxSupply() public view returns (uint256) {
+        return
+            (_MAX_SUPPLY_PER_PHASE_SOLDIERS +
+                _MAX_SUPPLY_PER_PHASE_COMMANDERS +
+                _MAX_SUPPLY_PER_PHASE_GENERALS) * _phase;
+    }
+
+    function enableNextPhase(
+        uint256 pricePerSoldier,
+        uint256 pricePerCommander,
+        uint256 pricePerGeneral
+    ) external onlyOwner {
+        if (_phase == _MAX_PHASES || _phasesLocked) revert MaxPhaseReached();
+
+        if (
+            _pricePerSoldier > pricePerSoldier ||
+            _pricePerCommander > pricePerCommander ||
+            _pricePerGeneral > pricePerGeneral
+        ) revert NextPhasePriceMustBeEqualOrHigher();
+
+        _phase += 1;
+        _pricePerSoldier = pricePerSoldier;
+        _pricePerCommander = pricePerCommander;
+        _pricePerGeneral = pricePerGeneral;
+    }
+
+    function lockPhases() external onlyOwner {
+        _phasesLocked = true;
     }
 }
